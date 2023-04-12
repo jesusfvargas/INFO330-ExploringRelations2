69c17891af709c7f623d35985c322f9947a51cec
Jesus Vargas <jfv97@uw.edu>
2023-04-11 23:26:27 -0700

    questions 1-4

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,7 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+SELECT e.FirstName, e.LastName, i.InvoiceId
+FROM employees AS e, customers AS c, invoices AS i
+WHERE e.EmployeeId = c.SupportRepId
+AND c.CustomerId = i.CustomerId
+AND e.Title = 'Sales Support Agent'
+
--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,10 @@
 -- Which sales agent made the most in sales in 2010?
+SELECT e.FirstName, e.LastName AS FullName, SUM(i.Total) AS TotalSales
+FROM employees AS e, customers AS c, invoices AS i
+WHERE e.EmployeeId = c.SupportRepId
+AND c.CustomerId = i.CustomerId
+AND e.Title = 'Sales Support Agent'
+AND strftime('%Y', i.InvoiceDate) = '2010'
+GROUP BY FullName
+ORDER BY TotalSales DESC
+LIMIT 1;
